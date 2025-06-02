8339a8efa5bb08856635a0e2d36b178c6e848abc
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-06-02 19:53:39 -0300

    Add:Primeiro Commit do porjeto, Criando a Classe Model

---
+++ src/java/br/com/prsoftware/model/Agendamento.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.prsoftware.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Paulo
+ * Criando o Model de Agendamento dos Veiculos 
+ */
+public class Agendamento {
+    private int id;
+    private String veiculo;
+    private Date dataRetirada;
+    private Date dataDevolucao;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(String veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public Date getDataRetirada() {
+        return dataRetirada;
+    }
+
+    public void setDataRetirada(Date dataRetirada) {
+        this.dataRetirada = dataRetirada;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+    
+    
+}
